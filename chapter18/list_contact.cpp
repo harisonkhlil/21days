e629f4954a1cd9739de4ef849a63f0bd15aa6475
JunFeng Su <harisonkhlil@gmail.com>
2023-06-30 11:27:11 +0800

    理解 operator

--- chapter18/list_contact.cpp
+++ chapter18/list_contact.cpp
@@ -28,6 +28,17 @@ struct ContactItem {
   bool operator<(const ContactItem &itemToCompare) const {
     return (this->name < itemToCompare.name);
   }
+  /*
+   * 理解 operator const char *() const { return displayAs.c_str(); }
+   * 1. operator const char *() const 是一个类型转换函数，它将 ContactItem
+   * 类型转换为 const char * 类型
+   * 2. const char * 是一个指针，指向一个字符串
+   * 3. displayAs 是一个 string 类型，它有一个成员函数 c_str()，返回一个 const
+   * char * 类型的指针
+   * 4. operator const char *() const { return displayAs.c_str(); } 的意思是将
+   * displayAs.c_str() 的返回值作为
+   * operator const char *() const 的返回值
+   */
   operator const char *() const { return displayAs.c_str(); }
 
   bool SortOnphoneNumber(const ContactItem &item1, const ContactItem &item2) {
